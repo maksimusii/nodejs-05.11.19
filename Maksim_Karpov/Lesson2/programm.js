c6ecad07ed6fb5199e4f512d74784d93e3d9a6b5
Maksim Karpov <maksimus@rtural.ru>
2019-11-23 01:19:10 +0500

    ddd

--- Maksim_Karpov/Lesson2/programm.js
+++ Maksim_Karpov/Lesson2/programm.js
@@ -1,14 +1,37 @@
 const readline = require('readline');
+const random = require('random');
+var player_side;
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-
+// coin_side = randome.boolean();
+console.log('Угадайте сторону упавшей монеты');
+console.log('Введите 1 если выбрали решку или 2 если выбрали орел');
 rl.on('line', (cmd) => {
-console.log(`Вы ввели: ${cmd}`);
-
-if (cmd === 'exit') {
-    rl.close()
+    if (cmd === 'exit') {
+        rl.close()
+    }
+console.log('Угадайте сторону упавшей монеты');
+console.log('Введите 1 если выбрали решку или 2 если выбрали орел');
+if (cmd === '1') {
+    player_side = true;
+    console.log('Вы загадали решку')
+} else if (cmd === '2') {
+    player_side = false,
+    console.log('Вы загадали орел')
+}else {
+    console.log('Вы загадали неправильно')
 }
+
+if (random.boolean() === player_side){
+    console.log('Вы угадали');
+} 
+
+if  (random.boolean() != player_side && player_side === true||false){
+    console.log('Вы не угадали');
+}
+
+
 });
